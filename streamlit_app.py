d03d1bf7fb6fbfc2da1d723dbae6bb7016d14bfd
J. Hathaway <hathawayj@byui.edu>
2023-11-29 11:59:08 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,19 @@ from collections import namedtuple
 import altair as alt
 import math
 import pandas as pd
+import polars as pl
+import pyarrow as pa
+import pyarrow.parquet as pq
 import streamlit as st
+import plotly.express as px
+
+# %%
+county = pl.read_parquet('data/active_members_county.parquet')
+tract = pl.read_parquet("data/active_members_tract.parquet")
+chapel_scrape = pl.read_parquet("data/full_church_building_data-20.parquet")
+chapel_safegraph = pl.read_parquet("data/safegraph_chapel.parquet")
+temples = pl.from_arrow(pq.read_table("data/temple_details_spatial.parquet"))
+tract_nearest = pl.from_arrow(pq.read_table("data/tract_distance_to_nearest_temple.parquet"))
 
 """
 # Welcome to Streamlit!
