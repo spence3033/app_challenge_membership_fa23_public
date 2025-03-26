5bda1b7e72f22ab257125c544d27543e70f6f281
spencer <sbirch3303@gmail.com>
2025-03-25 20:44:49 -0600

    trying new fix. polars.parquet.read_parquet()

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,9 +27,14 @@ tract_before = pl.read_parquet("data/active_members_tract.parquet")
 chapel_scrape = pl.read_parquet("data/full_church_building_data-20.parquet")
 chapel_safegraph = pl.read_parquet("data/safegraph_chapel.parquet")
 chapel_safegraph_clean = pl.read_parquet("data/safegraph_chapel_clean.parquet")
+
+# 03/25/2025 Fixing below two lines
 # temples = pl.from_arrow(pq.read_table("data/temple_details_spatial.parquet"))
-temples = pl.from_arrow(pq.read_parquet("data/temple_details_spatial.parquet"))
-tract_nearest = pl.from_arrow(pq.read_table("data/tract_distance_to_nearest_temple.parquet"))
+# tract_nearest = pl.from_arrow(pq.read_table("data/tract_distance_to_nearest_temple.parquet"))
+
+# 03/25/2025 Attempted fix below 
+temples = pq.read_table("data/temple_details_spatial.parquet")
+tract_nearest = pq.read_table("data/tract_distance_to_nearest_temple.parquet")
 
 # %%
 temples
